b232a25dee003ba713000d2d369a6ff9893921a7
Trey Tayler Smith <treytaylersmith@gmail.com>
2024-10-14 16:17:37 -0500

    tag routes finished, delete routes finished

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -47,7 +47,7 @@ router.post("/", async (req, res) => {
 router.put("/:id", async (req, res) => {
   // update a category by its `id` value
   try {
-    await Category.update(
+    const categoryData = await Category.update(
       {
         category_name: req.body.category_name,
       },
@@ -57,6 +57,10 @@ router.put("/:id", async (req, res) => {
         },
       }
     );
+   if(!categoryData){
+    res.status(404).json({message:"No Tag with that id"});
+      return
+   }
     res.status(200).json({message: "Category Updated Successfully"});
   } catch (err) {
     res.status(500).json(err);
@@ -66,15 +70,20 @@ router.put("/:id", async (req, res) => {
 router.delete("/:id", async (req, res) => {
   // delete a category by its `id` value
   try {
-    await Category.destroy({
+    const categoryData = await Category.destroy({
       where: {
         id: req.params.id,
       },
     });
+
+    if(!categoryData){
+      res.status(404).json({message:"No Category with that id"});
+      return
+    }
+
     res.status(200).json({message:"Category Deleted Successfully"});
   }catch(err) {
     res.status(500).json(err);
-  }
-});
+  }});
 
 module.exports = router;
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -48,6 +48,10 @@ router.get("/:id", async (req, res) => {
         },
       ],
     });
+    if(!productData){
+      res.status(404).json({message:"No Product with that id"});
+      return
+    }
     res.status(200).json(productData);
   } catch (err) {
     res.status(500).json(err);
@@ -130,8 +134,23 @@ router.put("/:id", (req, res) => {
     });
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete one product by its `id` value
-});
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if(!productData){
+      res.status(404).json({message:"No Product with that id"});
+      return
+    }
+
+    res.status(200).json({message:"Product Deleted Successfully"});
+  }catch(err) {
+    res.status(500).json(err);
+}});
 
 module.exports = router;
